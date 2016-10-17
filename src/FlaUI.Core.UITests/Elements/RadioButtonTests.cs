8196bccb97c76f5a8f76014fcdba09d702ffbef5
Roemer <r.baeriswyl@gmail.com>
2016-10-17 09:40:59 +0200

    Added

--- src/FlaUI.Core.UITests/Elements/RadioButtonTests.cs
+++ src/FlaUI.Core.UITests/Elements/RadioButtonTests.cs
@@ -9,7 +9,6 @@ namespace FlaUI.Core.UITests.Elements
     [TestFixture(AutomationType.UIA2, TestApplicationType.Wpf)]
     [TestFixture(AutomationType.UIA3, TestApplicationType.WinForms)]
     [TestFixture(AutomationType.UIA3, TestApplicationType.Wpf)]
-    [Ignore("Ignore for testing reasons")]
     public class RadioButtonTests : UITestBase
     {
         public RadioButtonTests(AutomationType automationType, TestApplicationType appType) : base(automationType, appType)
